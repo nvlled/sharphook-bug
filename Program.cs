6fe9f29d95868de6354de0967002db72faf1806a
Ronald Casili <nvlled@gmail.com>
2024-07-04 15:53:41 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using SharpHook.Native;
+// See https://aka.ms/new-console-template for more information
+
+
+UioHook.SetDispatchProc((ref UioHookEvent data, nint _) =>
+{
+    Console.WriteLine("event: {0} {1}", data.Time, data.Type);
+}, IntPtr.Zero);
+
+
+UioHook.Run();
